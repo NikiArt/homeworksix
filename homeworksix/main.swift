3332337d22067458a7b60c2579a859000a35314a
Nikita Boiko <genertyniki@gmail.com>
2019-10-25 08:36:49 +0300

    Initial Commit

---
+++ homeworksix/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  homeworksix
+//
+//  Created by Nikita Boiko on 25.10.2019.
+//  Copyright © 2019 Nikita Boiko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
